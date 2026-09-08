798199d27a70468e1e08e1343f6fd7529054449c
Wisnu Mulya <wisnu@wisnumulya.com>
2026-09-08 22:34:00 +0700

    Add: queries for staging and inserting tables

--- sql_queries.py
+++ sql_queries.py
@@ -49,7 +49,7 @@ CREATE TABLE staging_songs (
     artist_location VARCHAR(500),
     artist_name VARCHAR(500),
     song_id VARCHAR,
-    title VARCHAR,
+    title VARCHAR(500),
     duration FLOAT,
     year INT
 );
@@ -83,7 +83,7 @@ DISTSTYLE ALL;
 song_table_create = ("""
 CREATE TABLE songs (
     song_id VARCHAR PRIMARY KEY SORTKEY,
-    title VARCHAR,
+    title VARCHAR(500),
     artist_id VARCHAR,
     year INT,
     duration FLOAT
@@ -94,8 +94,8 @@ DISTSTYLE ALL;
 artist_table_create = ("""
 CREATE TABLE artists (
     artist_id VARCHAR PRIMARY KEY DISTKEY,
-    name VARCHAR SORTKEY,
-    location VARCHAR,
+    name VARCHAR(500) SORTKEY,
+    location VARCHAR(500),
     latitude FLOAT,
     longitude FLOAT
 );
@@ -117,27 +117,90 @@ DISTSTYLE ALL;
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+COPY staging_events FROM '{}'
+CREDENTIALS 'aws_iam_role={}'
+FORMAT AS JSON '{}'
+REGION 'us-west-2';
+""").format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'], config['S3']['LOG_JSONPATH'])
 
 staging_songs_copy = ("""
-""").format()
+COPY staging_songs FROM '{}'
+CREDENTIALS 'aws_iam_role={}'
+FORMAT AS JSON 'auto'
+REGION 'us-west-2';
+""").format(config['S3']['SONG_DATA'], config['IAM_ROLE']['ARN'])
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+SELECT
+    TIMESTAMP 'epoch' + e.ts/1000 * INTERVAL '1 second' AS start_time,
+    e.userId AS user_id,
+    e.level AS level,
+    s.song_id AS song_id,
+    s.artist_id AS artist_id,
+    e.sessionId AS session_id,
+    e.location AS location,
+    e.userAgent AS user_agent
+FROM staging_events e
+JOIN staging_songs s
+ON e.song = s.title
+AND e.artist = s.artist_name
+WHERE e.page = 'NextSong';
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+SELECT DISTINCT 
+    userId AS user_id,
+    firstName AS first_name,
+    lastName AS last_name,
+    gender AS gender,
+    level AS level
+FROM staging_events
+WHERE userId IS NOT NULL;
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+SELECT DISTINCT
+    song_id,
+    title,
+    artist_id,
+    year,
+    duration
+FROM staging_songs
+WHERE song_id IS NOT NULL;
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+SELECT DISTINCT
+    artist_id,
+    artist_name AS name,
+    artist_location AS location,
+    artist_latitude AS latitude,
+    artist_longitude AS longitude
+FROM staging_songs
+WHERE artist_id IS NOT NULL;
 """)
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+SELECT DISTINCT
+    start_time,
+    EXTRACT(hour FROM start_time) AS hour,
+    EXTRACT(day FROM start_time) AS day,
+    EXTRACT(week FROM start_time) AS week,
+    EXTRACT(month FROM start_time) AS month,
+    EXTRACT(year FROM start_time) AS year,
+    EXTRACT(weekday FROM start_time) AS weekday
+FROM songplays
+WHERE start_time IS NOT NULL;
 """)
+
+
 
 # QUERY LISTS
 
